e312f9566aabd0ace8b1acee7ee382624edef042
Phoebe Li <phoebe_li@toyotacredit.ca>
2019-04-23 17:04:51 -0400

    added code from tutorial before routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './feature/heroes/heroes.component';
+import { HeroDetailComponent } from './feature/hero-detail/hero-detail.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { MessagesComponent } from './feature/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientInMemoryWebApiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
